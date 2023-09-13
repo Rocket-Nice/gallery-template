32860c3760011201d8f273bd27df45aaf87c9817
Igor <igor.guchmazovwork@gmail.com>
2023-09-13 09:37:35 +0300

    [Add]: скрытие навигации слайдера при скролле на первом экране главной

--- src/js/components/MainSlider.js
+++ src/js/components/MainSlider.js
@@ -17,21 +17,44 @@ export default function MainSlider() {
     },
   });
 
-  const buttonNext = document.querySelectorAll('.main-slider__navigation .swiper-navigation-next');
-  const buttonPrev = document.querySelectorAll('.main-slider__navigation .swiper-navigation-prev');
-  if (buttonNext.length > 0) {
-    buttonNext.forEach(btn => {
-      btn.addEventListener('click', () => {
-        swiper.slideNext();
-      })
-    })
-  }
+  const slider = document.querySelector('.main-slider .swiper');
 
-  if (buttonPrev.length > 0) {
-    buttonPrev.forEach(btn => {
-      btn.addEventListener('click', () => {
-        swiper.slidePrev();
+  if (slider) {
+
+    const buttonNext = document.querySelectorAll('.main-slider__navigation .swiper-navigation-next');
+    const buttonPrev = document.querySelectorAll('.main-slider__navigation .swiper-navigation-prev');
+    if (buttonNext.length > 0) {
+      buttonNext.forEach(btn => {
+        btn.addEventListener('click', () => {
+          swiper.slideNext();
+        })
       })
-    })
+    }
+
+    if (buttonPrev.length > 0) {
+      buttonPrev.forEach(btn => {
+        btn.addEventListener('click', () => {
+          swiper.slidePrev();
+        })
+      })
+    }
+
+    const pagination = document.querySelector('.main-slider__pagination');
+    const navigation = document.querySelector('.main-slider__navigation');
+
+    function checkedScroll() {
+      if (window.innerWidth > 1439 && window.scrollY > 20) {
+        pagination.classList.add('--hidden');
+        navigation.classList.add('--hidden');
+      } else {
+        pagination.classList.remove('--hidden');
+        navigation.classList.remove('--hidden');
+      }
+    }
+
+    checkedScroll();
+    window.addEventListener('scroll', () => {
+      checkedScroll();
+    });
   }
 }
