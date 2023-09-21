1e8273f5a3af34b68d77066dd1c44990b5b21fe5
Igor <igor.guchmazovwork@gmail.com>
2023-09-21 17:54:17 +0300

    [Add]: верстка блока с бегущим текстом на странице О нас

---
+++ template-about.php
@@ -0,0 +1,106 @@
+<?php
+/*
+Template Name: About
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="smooth-wrapper">
+        <div id="smooth-content">
+            <main class="about-page">
+                <section class="about-desc">
+                    <div class="about-desc__bg">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/about-desc.jpg" width="1920" height="1080" loading="lazy" decoding= "async" alt="">
+                    </div>
+                    <div class="about-desc__container">
+                        <div class="about-desc__content">
+                            <div class="about-desc__subtitle">немного о</div>
+                            <div class="about-desc__title">GALLERY</div>
+                            <div class="about-desc__text">
+                                Распространенный в Западной Европе формат, главной особенностью которого является удобство и комфорт доступа непосредственно к магазинам, каждый из которых имеет отдельный вход с улицы.
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section class="about-marquee">
+                    <div class="about-marquee__block" data-marquee-wrapper>
+                        <div class="about-marquee__item" data-marquee="left">
+                            <a class="about-marquee__link border" href="#">Товары для дома</a>
+                            <a class="about-marquee__link white" href="#">Одежда</a>
+                            <a class="about-marquee__link border" href="#">Развлечения</a>
+                            <a class="about-marquee__link white" href="#">Суши</a>
+                            <a class="about-marquee__link border" href="#">Рестораны</a>
+                            <a class="about-marquee__link white" href="#">Кино</a>
+                            <a class="about-marquee__link border" href="#">Бытовая техника</a>
+                        </div>
+                        <div class="about-marquee__item" data-marquee="left">
+                            <a class="about-marquee__link white" href="#">Товары для дома</a>
+                            <a class="about-marquee__link border" href="#">Одежда</a>
+                            <a class="about-marquee__link white" href="#">Развлечения</a>
+                            <a class="about-marquee__link border" href="#">Суши</a>
+                            <a class="about-marquee__link white" href="#">Рестораны</a>
+                            <a class="about-marquee__link border" href="#">Кино</a>
+                            <a class="about-marquee__link white" href="#">Бытовая техника</a>
+                        </div>
+                        <div class="about-marquee__item" data-marquee="left">
+                            <a class="about-marquee__link border" href="#">Товары для дома</a>
+                            <a class="about-marquee__link white" href="#">Одежда</a>
+                            <a class="about-marquee__link border" href="#">Развлечения</a>
+                            <a class="about-marquee__link white" href="#">Суши</a>
+                            <a class="about-marquee__link border" href="#">Рестораны</a>
+                            <a class="about-marquee__link white" href="#">Кино</a>
+                            <a class="about-marquee__link border" href="#">Бытовая техника</a>
+                        </div>
+                    </div>
+                    <div class="about-marquee__block" data-marquee-wrapper>
+                        <div class="about-marquee__item" data-marquee="right">
+                            <a class="about-marquee__link white" href="#">Суши</a>
+                            <a class="about-marquee__link border" href="#">Бытовая техника</a>
+                            <a class="about-marquee__link white" href="#">Одежда</a>
+                            <a class="about-marquee__link border" href="#">Товары для дома</a>
+                            <a class="about-marquee__link white" href="#">Зоотовары</a>
+                            <a class="about-marquee__link border" href="#">Рестораны</a>
+                            <a class="about-marquee__link white" href="#">Кино</a>
+                        </div>
+                        <div class="about-marquee__item" data-marquee="right">
+                            <a class="about-marquee__link border" href="#">Суши</a>
+                            <a class="about-marquee__link white" href="#">Бытовая техника</a>
+                            <a class="about-marquee__link border" href="#">Одежда</a>
+                            <a class="about-marquee__link white" href="#">Товары для дома</a>
+                            <a class="about-marquee__link border" href="#">Зоотовары</a>
+                            <a class="about-marquee__link white" href="#">Рестораны</a>
+                            <a class="about-marquee__link border" href="#">Кино</a>
+                        </div>
+                        <div class="about-marquee__item" data-marquee="right">
+                            <a class="about-marquee__link white" href="#">Суши</a>
+                            <a class="about-marquee__link border" href="#">Бытовая техника</a>
+                            <a class="about-marquee__link white" href="#">Одежда</a>
+                            <a class="about-marquee__link border" href="#">Товары для дома</a>
+                            <a class="about-marquee__link white" href="#">Зоотовары</a>
+                            <a class="about-marquee__link border" href="#">Рестораны</a>
+                            <a class="about-marquee__link white" href="#">Кино</a>
+                        </div>
+                    </div>
+                </section>
+                <section class="about-rates">
+                    <div class="about-rates__bg">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/about-rates.jpg" width="1920" height="1080" loading="lazy" decoding= "async" alt="">
+                    </div>
+                    <div class="about-rates__container">
+                        <div class="about-rates__title">100</div>
+                        <div class="about-rates__subtitle">машиномест на подземном паркинге</div>
+                        <a href="#" class="dark-btn">
+                            <div class="dark-btn__text">Иформация о тарифах</div>
+                            <div class="dark-btn__icon">
+                                <img src="<?php bloginfo('template_url'); ?>/assets/icons/btn-arrow.svg" width="24" height="24" loading="lazy" decoding= "async" alt="">
+                            </div>
+                        </a>
+                    </div>
+                </section>
+            </main>
+            <?php get_footer(); ?>
+        </div>
+    </div>
+</body>
+</html>
