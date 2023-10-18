0c90d58172032345119f11448072b0ee7fd474be
Igor <igor.guchmazovwork@gmail.com>
2023-10-18 16:56:37 +0300

    [Fix]: плавный стролл

--- src/js/components/SmoothScroll.js
+++ src/js/components/SmoothScroll.js
@@ -5,7 +5,12 @@ import { ScrollSmoother } from '../libs/ScrollSmoother.min.js';
 export default function SmoothScroll() {
   gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-  ScrollSmoother.create({
-    smooth: 1,
-  })
+  const mainPage = document.querySelector('main.main-page');
+  const aboutPage = document.querySelector('main.about-page');
+
+  if (mainPage || aboutPage) {
+    ScrollSmoother.create({
+      smooth: 1,
+    })
+  }
 }
