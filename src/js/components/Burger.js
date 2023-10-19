7f643409db47abf8c0a105832c8e2342082cfba7
Igor <igor.guchmazovwork@gmail.com>
2023-10-19 11:32:27 +0300

    [Fix]: цвет лого при открытом бургере

--- src/js/components/Burger.js
+++ src/js/components/Burger.js
@@ -3,15 +3,18 @@ export default function Burger() {
   const burger = document.querySelector('[data-burger]');
   const burgerOpen = document.querySelector('[data-burger-open]');
   const burgerClose = document.querySelector('[data-burger-close]');
+  const logo = document.querySelector('.header-logo');
 
   if (burger) {
     burgerOpen.addEventListener('click', () => {
       burger.classList.add('--open');
       body.classList.add('hidden');
+      logo.classList.add('--burger');
     });
     burgerClose.addEventListener('click', () => {
       burger.classList.remove('--open');
       body.classList.remove('hidden');
+      logo.classList.remove('--burger');
     });
   }
 }
